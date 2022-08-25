f74734d3f29622334e1ce818a2d1817d12dd06da
Alex Berezov <53948974+Alex-Berezov@users.noreply.github.com>
2022-08-25 23:42:38 +0400

    Added test
    Added test

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import GlobalStyle from '../styles/globalstyles'
 import { theme } from '../styles/Theme/commonTheme.js'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
+const test = true
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
   cache: new InMemoryCache(),
