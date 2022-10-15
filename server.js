879a81eb0b20bae486d6095243295f260a0f75c0
Aleksey Berezov <a.berezov@leningrad.io>
2022-10-15 12:13:23 +0400

    added server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import next from 'next'
+
+const port = 3000;
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:${port}');
+  });
+});
