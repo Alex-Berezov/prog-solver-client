9deb98167fccd7c50a9bb50bcf250ea072720432
Aleksey Berezov <a.berezov@leningrad.io>
2022-10-23 18:32:56 +0400

    Added Google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/globalstyles'
 import { theme } from '../styles/Theme/commonTheme.js'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import Script from 'next/script'
 
 export const client = new ApolloClient({
   uri: process.env.NODE_ENV === 'production'
@@ -12,11 +13,23 @@ export const client = new ApolloClient({
 
 export default function App({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ApolloProvider>
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-10DERW20YY"/>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-10DERW20YY');
+        `}
+      </Script>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
+    </>
   )
 }
