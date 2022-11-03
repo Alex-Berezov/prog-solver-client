9821b0a345dd2cc2ff5843c8c3e881a03bf09401
Aleksey Berezov <a.berezov@leningrad.io>
2022-11-03 10:08:43 +0400

    added google-site-verification

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="google-site-verification" content="lhi7QMw2TjULX_yhm8j_N86X0MiL7chbOD4HvTciM_U" />
         <link rel="stylesheet" href="/fonts/fonts.css" />
       </Head>
       <body>
